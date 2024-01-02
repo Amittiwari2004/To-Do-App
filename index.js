7e1fb3c80bcda1cd7fc427b7162a9c09be02d63e
Amit Tiwari <139469702+Amittiwari2004@users.noreply.github.com>
2024-01-02 13:56:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const inputBox=document.getElementById("input-box");
+const List=document.getElementById("list-contanier");   
+
+function addTask(){
+
+    if(inputBox.value===''){
+        alert("Write Something");
+    }
+    else{
+       let li=document.createElement("li");
+       li.innerHTML=inputBox.value;
+       List.appendChild(li);
+       let span=document.createElement("span");
+       span.innerHTML="\u00d7";
+       li.appendChild(span);
+    }
+    inputBox.value='';
+    saveData();
+}
+
+List.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",List.innerHTML)
+}
+function showTask(){
+    List.innerHTML=localStorage.getItem("data");
+
+}
+showTask()
